defeeb2fabe8bfaa2b5100f65438ca31b61291c5
SREERAJ VR <sreerajvr172001@gmail.com>
2026-02-27 19:06:31 +0530

    fix: return 0 PWM for ticks/sec less than 1
    - to avoid stalling and unnecessary consumption of power
    - to stop the motor if ticks/sec is zero
    - to avoid oscillation around 0 at very low speeds

--- src/main.cpp
+++ src/main.cpp
@@ -132,6 +132,9 @@ void apply_pwm_to_motor(int pwm, int in1, int in2, int ch)
 int ticks_to_pwm(float ticks) 
 {
   float abs_ticks = fabs(ticks);
+  
+  if(abs_ticks < 1.0f) return 0; // return 0 PWM if ticks are very low to avoid motor stalling and unnecessary power consumption at very low speeds and also to stop the motor when the setpoint is zero or close to zero, which can help with stability and reduce wear on the motor. This also creates a small deadzone around zero to prevent oscillations at low speeds.
+
   float ratio = (float)abs_ticks / MAX_TICKS_PER_SEC;
   int p = (int)round(ratio * (max_pwm - min_pwm) + min_pwm); // Linear mapping from ticks to pwm range with min_pwm offset to ensure motion at low speeds
   
@@ -140,7 +143,7 @@ int ticks_to_pwm(float ticks)
   // Clamp pwm to the min and max range
   if (p > max_pwm) p = max_pwm;
   if (p < -max_pwm) p = -max_pwm;
-  
+
   return p;
 }
 
